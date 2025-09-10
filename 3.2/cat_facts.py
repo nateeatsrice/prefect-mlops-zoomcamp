b97ead69d540504f25d48a90367e71800df4ceeb
Nathaniel Rice-Canetto <nathaniel.c.rice@gmail.com>
2025-09-10 20:10:40 +0000

    updated .toml file to get prefect working

--- 3.2/cat_facts.py
+++ 3.2/cat_facts.py
@@ -1,7 +1,7 @@
 import httpx
 from prefect import flow, task
 
-
+# will try up to 4 times with a short time delay inbetween, any print statements will be in the logs
 @task(retries=4, retry_delay_seconds=0.1, log_prints=True)
 def fetch_cat_fact():
     cat_fact = httpx.get("https://f3-vyx5c2hfpq-ue.a.run.app/")
@@ -10,7 +10,7 @@ def fetch_cat_fact():
         raise Exception()
     print(cat_fact.text)
 
-
+# simple flow that just calls a task
 @flow
 def fetch():
     fetch_cat_fact()
